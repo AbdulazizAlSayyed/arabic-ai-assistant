158fb800bd0ac0dc1f804020e58fb86c8bf34150
mkojoko <mkojok91@gmail.com>
2026-03-25 15:30:23 +0200

    Switch to MarianMT for translation functionality

--- modules/translation.py
+++ modules/translation.py
@@ -1,16 +1,26 @@
-#import library
-from transformers import pipeline
+from transformers import MarianMTModel, MarianTokenizer
 
-#create translators
-translator_ar_en = pipeline("translation", model = "Helsinki-NLP/opus-mt-ar-en") 
-translator_en_ar = pipeline("translation", model = "Helsinki-NLP/opus-mt-en-ar")
+# load models once
+model_name_ar_en = "Helsinki-NLP/opus-mt-ar-en"
+tokenizer_ar_en = MarianTokenizer.from_pretrained(model_name_ar_en)
+model_ar_en = MarianMTModel.from_pretrained(model_name_ar_en)
 
-#translating functions
+model_name_en_ar = "Helsinki-NLP/opus-mt-en-ar"
+tokenizer_en_ar = MarianTokenizer.from_pretrained(model_name_en_ar)
+model_en_ar = MarianMTModel.from_pretrained(model_name_en_ar)
+
+
 def translate_text(text, source_lang="ar", target_lang="en"):
-  if source_lang == "ar" and target_lang == "en":
-    result = translator_ar_en(text)
-    return result[0]["translation_text"]
-  if source_lang == "en" and target_lang == "ar":
-    result = translator_en_ar(text)
-    return result[0]["translation_text"]
+
+    if source_lang == "ar" and target_lang == "en":
+        batch = tokenizer_ar_en([text], return_tensors="pt", padding=True)
+        generated = model_ar_en.generate(**batch)
+        return tokenizer_ar_en.decode(generated[0], skip_special_tokens=True)
+
+    if source_lang == "en" and target_lang == "ar":
+        text = ">>ara<< " + text
+        batch = tokenizer_en_ar([text], return_tensors="pt", padding=True)
+        generated = model_en_ar.generate(**batch)
+        return tokenizer_en_ar.decode(generated[0], skip_special_tokens=True)
+
     return text
